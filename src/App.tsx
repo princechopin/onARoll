7abdc46d145e66bf246bd778efbeeffd7375971b
princechopin <53460280+princechopin@users.noreply.github.com>
2022-07-03 19:47:53 -0500

    Front-end exercise for onARoll.

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,49 @@
 import React, { useState }  from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
 import dayjs from 'dayjs';
 
 const nasa_apod_url = "https://api.nasa.gov/planetary/apod";
-const nasa_api_key = "AJS9iXajqgCTgo96AbxwotypteOL083FAJitPekb";
+const nasa_api_key = "wOPSsUDdQBo40ijprU3J7jNnRmB8iVxejJg0Scuu";
 
 export function PickADate(): JSX.Element {
-  const [date, setDate] = React.useState(dayjs('2022-07-01'));
-  const [explanation, setExplanation] = React.useState('');
-
+  const [date, setDate] = React.useState(dayjs());
+  const [explanation, setExplanation] = React.useState("");
+  const [imageUrl, setImageUrl] = React.useState("");
   const onChangeDate = React.useCallback(
-   (e: React.ChangeEvent<HTMLInputElement>) => { 
+   (e: React.ChangeEvent<HTMLInputElement>) => {
      setDate(dayjs(e.target.value)); },
      [setDate]
   );
-  let apod_response = {url: "", explanation: ""};
-  let apod_url = "";
-  let apod_explanation = "";
+
+  let apodResponse = {url: "", explanation: "", error: {code:0, message:""}}
+  const onCreateWindow = React.useCallback(
+    () => { window.open(imageUrl, 'popUpWindow'); },
+    [imageUrl]
+  );
 
   let xhr = new XMLHttpRequest();
-  xhr.open("GET", nasa_apod_url+"?api_key="+nasa_api_key+"&date="+date.format("YYYY-MM-DD"));
+  let requestApi = nasa_apod_url+"?api_key="+nasa_api_key+"&date="+date.format("YYYY-MM-DD");
+  xhr.open("GET", requestApi);
   xhr.send();
-  xhr.onload = () => { 
-    apod_response = JSON.parse(xhr.responseText);
-    apod_url = apod_response.url;
-    setExplanation(apod_response.explanation);
-    window.open(apod_url, '_blank');
-  };
-  console.log(apod_explanation);
+  xhr.onload =
+    () => {
+      apodResponse = JSON.parse(xhr.responseText);
+      if (apodResponse.hasOwnProperty("error")) {
+        setExplanation(apodResponse.error.message);
+      } else {
+        setExplanation(apodResponse.explanation)
+        setImageUrl(apodResponse.url);
+      }
+    };
+
   return (
-    <div className="app__container">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-explanation"> Explanation: </h1>
-      <h1 className="App-explanationBody"> { explanation } </h1>
-      <h4 className="app__title">
-	Picked Date: {date.format("DD - MMMM - YYYY")}
-      </h4>
-      <input type="date" onChange={onChangeDate}
-      ></input>
+    <div className="App-layout">
+      <input type="image" className="App-logo1" onClick={onCreateWindow} src={imageUrl} alt={logo} /> 
+      <h1 className="App-explanation"> Explanation </h1>
+      <p className="App-explanationBody"> { explanation } </p>
+      <h1 className="App-explanation"> Picked Date: {date.format("DD - MMMM - YYYY")} </h1>
+      <input type="date" onChange={onChangeDate} value={date.format("YYYY-MM-DD")} />       
     </div>
   );
 };
@@ -48,7 +53,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         OnARoll frontend exercise
-      	<PickADate/>
+      <PickADate/>
       </header>
     </div>
   );
