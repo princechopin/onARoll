35bee5d04a348766e75646b077341a98e8526f13
princechopin <princechopin@gmail.com>
2022-07-07 00:28:06 -0500

    Add title to the layout.

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,31 @@ import dayjs from 'dayjs';
 
 const nasa_apod_url = "https://api.nasa.gov/planetary/apod";
 const nasa_api_key = "wOPSsUDdQBo40ijprU3J7jNnRmB8iVxejJg0Scuu";
+type ErrorDefiniton = Readonly<{
+  code: number;
+  message: string;
+}>;
+
+type ApodResponseDefinition = Readonly<{
+  explanation: string;
+  url: string;
+  title: string;
+  error?: ErrorDefiniton;
+}>;
 
 export function PickADate(): JSX.Element {
+  let apodResponse: ApodResponseDefinition = {url: "", explanation: "", title: ""};
   const [date, setDate] = React.useState(dayjs());
   const [explanation, setExplanation] = React.useState("");
   const [imageUrl, setImageUrl] = React.useState("");
+  const [title, setTitle] = React.useState("");
   const onChangeDate = React.useCallback(
    (e: React.ChangeEvent<HTMLInputElement>) => {
      setDate(dayjs(e.target.value)); },
      [setDate]
   );
 
-  let apodResponse = {url: "", explanation: "", error: {code:0, message:""}}
+  
   const onCreateWindow = React.useCallback(
     () => { window.open(imageUrl, 'popUpWindow'); },
     [imageUrl]
@@ -29,19 +42,19 @@ export function PickADate(): JSX.Element {
   xhr.onload =
     () => {
       apodResponse = JSON.parse(xhr.responseText);
-      if (apodResponse.hasOwnProperty("error")) {
-        setExplanation(apodResponse.error.message);
-      } else {
-        setExplanation(apodResponse.explanation)
-        setImageUrl(apodResponse.url);
-      }
+      setTitle(apodResponse.title);
+      setExplanation(apodResponse.explanation);
+      setImageUrl(apodResponse.url);
     };
 
   return (
     <div className="App-layout">
+      <h1 className="App-title"> {title ? title : "No Title"} </h1>
       <input type="image" className="App-logo1" onClick={onCreateWindow} src={imageUrl} alt={logo} /> 
-      <h1 className="App-explanation"> Explanation </h1>
-      <p className="App-explanationBody"> { explanation } </p>
+      <h1 className="App-title"> Explanation </h1>
+      <p className="App-explanation"> { 
+        apodResponse.hasOwnProperty("error") ? apodResponse.error?.message : explanation
+      } </p>
       <h1 className="App-explanation"> Picked Date: {date.format("DD - MMMM - YYYY")} </h1>
       <input type="date" onChange={onChangeDate} value={date.format("YYYY-MM-DD")} />       
     </div>
